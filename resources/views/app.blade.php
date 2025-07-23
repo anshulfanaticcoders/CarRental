992b955fa7aabb17d334ecb1263fc1eaba4a1fca
anshulfanaticcoders <anshul@fanaticcoders.com>
2025-07-23 17:40:35 +0530

    make changes as client said

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -70,7 +70,7 @@
     </script>
 
     <!--Start of Tawk.to Script-->
-    <!-- <script type="text/javascript">
+    <script type="text/javascript">
         var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         (function () {
             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
@@ -80,7 +80,7 @@
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
         })();
-    </script> -->
+    </script>
     <!--End of Tawk.to Script-->
 
     <script src="https://script.tapfiliate.com/tapfiliate.js" type="text/javascript" async></script>
