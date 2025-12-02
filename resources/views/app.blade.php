62f6e3d2fba1e3b39d909cb7b5c725caf395c747
anshulfanaticcoders <anshul@fanaticcoders.com>
2025-12-02 16:50:34 +0530

    add microsoft clarify

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -94,6 +94,18 @@
   tap('detect');
 </script>
 
+    {{-- Microsoft Clarity Analytics - Production Only --}}
+  @if(env('CLARITY_ENABLED', false) && app()->environment('production') && !empty(env('CLARITY_PROJECT_ID')))
+    <!-- Microsoft Clarity Analytics -->
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "{{ env('CLARITY_PROJECT_ID') }}");
+    </script>
+    @endif
+
     @inertiaHead
 </head>
 
